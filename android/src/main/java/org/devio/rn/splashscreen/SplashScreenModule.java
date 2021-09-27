a9a7745c669f516c02b63eff5855958e3682c773
lehaBay <fominleha@gmail.com>
2021-09-27 22:06:14 +0400

    Revert "add event when splashscreen is started loading to fix error with animation on top of splashscreen"
    This reverts commit 222a8e84e9ee5a2847732ae056807bf5ddbc7101.

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreenModule.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreenModule.java
@@ -19,8 +19,6 @@ public class SplashScreenModule extends ReactContextBaseJavaModule{
 
     public SplashScreenModule(final ReactApplicationContext reactContext) {
         super(reactContext);
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                                  .emit("SPLASHSCREEN_LOADING", params);
         Runnable callback = new Runnable() {
 
           public void run() {
@@ -28,6 +26,7 @@ public class SplashScreenModule extends ReactContextBaseJavaModule{
             reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit("SPLASHSCREEN_DONE", params);
           };
+
         };
         SplashScreen.setFinishedAnimationCallback(callback);
     }
